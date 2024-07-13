7f66e3ed8058f9775adf192565cac6e20759cf23
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-07-13 14:48:55 +0200

    Initial Commit

---
+++ Restart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Dorian Emenir on 13/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
