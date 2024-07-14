867f2c9cfa9ddfe0d2ceff1381bcd6cd76aa589e
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-07-14 19:14:11 +0200

    Create: HomeView with that updates the @AppStorage - isOnboardingViewActive value when clicked 👷🏻‍♂️

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -5,14 +5,31 @@
 //  Created by Dorian Emenir on 14/07/2024.
 //
 
+// MARK: - IMPORTS
 import SwiftUI
 
+// MARK: HomeView
 struct HomeView: View {
+    // MARK: - PROPERTY
+    /// Code safety: The following line of code will be skipped if the program doesn't find the `ONBOARDING` key previously set in the device's permanent storage
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    
+    // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Home")
+                .font(.largeTitle)
+            
+            Button(action: {
+                isOnboardingViewActive = true
+            }, label: {
+                Text("Restart")
+            })
+        } //: VSTACK
     }
 }
 
+// MARK: - PREVIEW
 #Preview {
     HomeView()
 }
