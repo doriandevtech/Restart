e51c5af5fc851535fac0f1a501aae3e8fb973dd0
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-07-14 19:12:13 +0200

    Create: HomeView and OnboardingView 🗂️

---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Dorian Emenir on 14/07/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Restart/Screens/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by Dorian Emenir on 14/07/2024.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
